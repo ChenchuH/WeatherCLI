cbe434eabbcd5ae730ce560f838324c11fbedb2d
ChenchuH <ynchenchuh@icloud.com>
2026-03-25 22:38:17 -0700

    Fixed hard coded value of location left in

--- src/main.py
+++ src/main.py
@@ -8,14 +8,14 @@ if not query.lower().startswith("weather at "):
     print("Use: Weather at <location>")
     exit()
 
-location = query[11:].strip()  # everything after "weather at "
+location = query[11:].strip()
 
 
 with Progress(SpinnerColumn(), TextColumn("[cyan]Fetching...")) as progress:
 
     task = progress.add_task("work", total=2)
 
-    lat, long = get_location_data("Torrance, CA 90505")
+    lat, long = get_location_data(location)
     progress.update(task, advance=1)
 
     weather = get_weather_data(lat,long)
@@ -24,4 +24,4 @@ with Progress(SpinnerColumn(), TextColumn("[cyan]Fetching...")) as progress:
 temp = weather["current"]["temperature_2m"]
 unit = weather["current_units"]["temperature_2m"]
 
-print(f"Currently ~{temp}{unit}")
+print(f"Currently ~{temp}{unit} @ {location}")

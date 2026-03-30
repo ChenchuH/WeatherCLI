4e2888638e3ebe4b7a74095b9f4827ac26b23e42
ChenchuH <ynchenchuh@icloud.com>
2026-03-29 23:48:56 -0700

    created sparklines for the next 24 hours

--- src/weathercli/helpers/sparklines.py
+++ src/weathercli/helpers/sparklines.py
@@ -3,7 +3,7 @@
 import requests
 
 def get_weather_data():
-    res = requests.get("https://api.open-meteo.com/v1/forecast", params={"latitude":"33.8369", "longitude":"118.3407", "current": "temperature_2m"}, timeout=5)
+    res = requests.get("https://api.open-meteo.com/v1/forecast", params={"latitude":"33.8369", "longitude":"-118.3407", "hourly": "temperature_2m"}, timeout=5)
     #Torrance Cords for testing
     data = res.json()
     return data
@@ -18,10 +18,15 @@ def sparklines(temp_range_day):
     
     return "".join(sparklines_graph[int((x - mn) / (mx - mn) * (len(sparklines_graph) - 1))] for x in temp_range_day)
 
-temps = [15.2, 14.8, 14.3, 13.9, 14.5, 16.0, 17.2, 18.0]
-#temporary data
+data = get_weather_data()
 
+times = data["hourly"]["time"]
+temps = data["hourly"]["temperature_2m"][:24]
+
+#temporary data
+print("the next 24 hours")
 s=sparklines(temps)
 print(s)
+
 
 

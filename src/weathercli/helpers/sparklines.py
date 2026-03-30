25576dba945a18f83d49e3ae26a441cc0dd962be
ChenchuH <ynchenchuh@icloud.com>
2026-03-29 23:37:53 -0700

    setting up initial res request for testing

--- src/weathercli/helpers/sparklines.py
+++ src/weathercli/helpers/sparklines.py
@@ -1,5 +1,12 @@
 # Copyright (c) 2026 Chenchu Hemanth Yakasiri Saravanan
 # Licensed under the MIT License
+import requests
+
+def get_weather_data():
+    res = requests.get("https://api.open-meteo.com/v1/forecast", params={"latitude":"33.8369", "longitude":"118.3407", "current": "temperature_2m"}, timeout=5)
+    #Torrance Cords for testing
+    data = res.json()
+    return data
 
 
 def sparklines(temp_range_day):
@@ -12,8 +19,9 @@ def sparklines(temp_range_day):
     return "".join(sparklines_graph[int((x - mn) / (mx - mn) * (len(sparklines_graph) - 1))] for x in temp_range_day)
 
 temps = [15.2, 14.8, 14.3, 13.9, 14.5, 16.0, 17.2, 18.0]
-s=sparklines(temps)
+#temporary data
 
+s=sparklines(temps)
 print(s)
 
 
